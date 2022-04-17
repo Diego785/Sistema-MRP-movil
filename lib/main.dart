6d773400705685c8c892ae18dfc1ee1ae49e1550
DiegoHV <diegohurtado2412@gmail.com>
2022-04-17 14:30:55 -0400

    start

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'SISTEMA MRP',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const Center(
-        child: Text('MAIN PAGE SYSTEM MRP'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("SYSTEM MRP"),
+        ),
+        backgroundColor: Colors.white,
+        body: const Center(
+          child: Text('MAIN PAGE SYSTEM MRP'),
+        ),
       ),
     );
   }
