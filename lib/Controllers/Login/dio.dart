773e44e87621cbab504b18912fbecc8a27b0b5b7
Diego785 <diegohurtado2412@gmail.com>
2022-05-18 21:54:51 -0400

    virtual domain

--- lib/Controllers/Login/dio.dart
+++ lib/Controllers/Login/dio.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 Dio dio() {
   Dio dio = new Dio();
 
-  dio.options.baseUrl = "http://192.168.100.4/Sistema-MRP/public/api";
+  dio.options.baseUrl = "http://sistema-mrp.test/api";
   dio.options.headers['accept'] = 'Application/Json';
   return dio;
 }
