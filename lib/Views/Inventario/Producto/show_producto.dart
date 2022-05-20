ff39ad84772dc1f432ed361fe73dd8c2d750511a
Darwin Mamani <darwin_xd10@hotmail.com>
2022-05-20 16:41:53 -0400

    darwin, crud de producto

--- lib/Views/Inventario/Producto/show_producto.dart
+++ lib/Views/Inventario/Producto/show_producto.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:sistema_mrp/Models/models.dart';
+import 'dart:convert' as convert;
 
 class ProductoScreen extends StatefulWidget {
   @override
@@ -11,81 +12,74 @@ class ProductoScreen extends StatefulWidget {
 
 //------------------------------------------------------------------------------
 class _ProductoScreenState extends State<ProductoScreen> {
-  late Future<List<Producto>> _listProductos;
-
-  Future<List<Producto>> _getProducto() async {
-    Uri url = Uri.parse('http://10.0.2.2/Sistema-MRP/public/api/productos');
-    final response = await http.get(url);
-    print("RESPUESTA GET: :  Views.Producto._getProducto => " + response.body);
-        print('------------------------');
-
-    print(utf8.decode(response.bodyBytes));
-    print('------------------------');
-
-    List<Producto> data = [];
-
-    // if (response.statusCode == 200) {
-    //   String body = utf8.decode(response.bodyBytes);
-    //   final jsonData = jsonDecode(body);
-    //   for (var item in jsonData) {
-    //     print(item["id"]);
-    //     print(item["nombre"]);
-    //     data.add(MateriaPrima());
-    //   }
-    return data;
-    // } else {
-    //   throw Exception("Falló la conexión");
-    // }
-  }
+  List<Producto> listaDeProductos = [];
 
   @override
   void initState() {
     super.initState();
-    _listProductos = _getProducto();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // drawer: NavBar(),
       appBar: AppBar(
         title: const Text("Producto Crud"),
         backgroundColor: Colors.green.shade800,
       ),
-      body: FutureBuilder(
-        future: _listProductos,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            // return  ListView(
-            //     children: _list(context, snapshot.data),
-            //   );
-            return Text('snapshot.hasData = TRUE');
-          } else if (snapshot.hasError) {
-            return Text("Error");
-          }
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.green.shade700,
-        onPressed: () {
-          /*Navigator.push(
-                  context, MaterialPageRoute(builder: (_) => RegisterBus()))
-              .then((newBus) {
-            if (newBus != null) {
-              setState(() {
-                buses.add(newBus);
-                messageResponse(
-                    context, newBus.codigo.toString() + " has been saved!");
-              });
-            }
-          });*/
-        },
-        tooltip: "Agregar Productos",
-        child: Icon(Icons.add),
-      ),
+      body: listarProductos(),
+      // floatingActionButton: FloatingActionButton(
+      //   backgroundColor: Colors.green.shade700,
+      //   onPressed: () => cargarProductos(),
+      //   tooltip: "Agregar Productos",
+      //   child: const Icon(Icons.add),
+      // ),
     );
+  }
+
+  Widget listarProductos() {
+    return FutureBuilder(
+      future: cargarProductos(),
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        if (!snapshot.hasData) {
+          return Container(
+            child: ListTile(title: Text('No hay Consultas')),
+          );
+        }
+        // return Text('de momento paso listar productos');
+        return ListView(
+          children: getListaDeProductos(snapshot.data, context),
+        );
+      },
+    );
+  }
+
+  List<Widget> getListaDeProductos(List<dynamic>? data, BuildContext context) {
+    final List<Widget> listaDeWidget = [];
+
+    data?.forEach((element) {
+      final widgetTemp = ListTile(
+        title: Text(element.nombre),
+        onTap: () {
+          // Navigator.pushNamed(context, 'Producto', arguments: element);
+        },
+      );
+      listaDeWidget.add(widgetTemp);
+      listaDeWidget.add(Divider());
+    });
+    return listaDeWidget;
+  }
+
+  Future<List<dynamic>>  cargarProductos() async {
+    var response = await http
+        .get(Uri.parse('http://10.0.2.2/Sistema-MRP/public/api/productos'));
+    print("RESPUESTA GET: :  Views.Producto._getProducto => " + response.body);
+    var jsonResponse = convert.jsonDecode(response.body)['data'];
+    for (Map<String, dynamic> item in jsonResponse) {
+      Producto product = Producto.fromMap(item);
+      print(product);
+      listaDeProductos.add(product);
+    }
+    print('salio del for');
+    return listaDeProductos;
   }
 }
